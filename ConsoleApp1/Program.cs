8c9b5922393976601a415c1fd756bf2b0b423bdc
iwxnek <iwxnek@gmail.com>
2026-02-06 00:14:24 +0100

    fibonacci

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj numer wyrazu ciagu Fibonacciego:");
+        if (long.TryParse(Console.ReadLine(), out long n) && n >= 0)
+        {
+            int lastdigit = LastFibNumber(n);
+            Console.WriteLine($"Ostatnia cyfra tego wyrazu to:" + lastdigit);
+        }
+        else
+        {
+            Console.WriteLine("Podaj poprawna liczbe calkowita nieujemna");
+        }
+    }
+
+    static int LastFibNumber(long n)
+    {
+        int nlast = (int)(n % 60);
+        if (nlast == 0) return 0;
+        if (nlast == 1) return 1;
+
+        int a = 0;
+        int b = 1;
+        int wynik = 0;
+        for (int i = 2; i <= nlast; i++)
+        {
+            wynik = (a + b) % 10;
+            a = b;
+            b = wynik;
+        }
+
+        return nlast == 1 ? 1 : wynik;
+    }
+
+}
