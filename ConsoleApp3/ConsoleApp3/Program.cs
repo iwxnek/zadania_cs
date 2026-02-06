ff8312d675a3864d828cceb67320d677ac9acd63
iwxnek <iwxnek@gmail.com>
2026-02-06 15:06:58 +0100

    another one

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj zdanie");
+        string input = Console.ReadLine();
+        
+        Dictionary<char, int> litery = new Dictionary<char, int>();
+        foreach (char litera in input.ToLower())
+        {
+            if ((char.IsLetter(litera)))
+            {
+                if (litery.ContainsKey(litera))
+                {
+                    litery[litera]++;
+                }
+                else
+                {
+                    litery[litera] = 1;
+                }
+            }
+        }
+        Console.WriteLine("Wystepowanie liter:");
+        foreach (KeyValuePair<char, int> item in litery)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+}
