ac47d9f327a795465f577be26cce3025ecec6821
iwxnek <iwxnek@gmail.com>
2026-02-06 14:24:09 +0100

    new

---
+++ ConsoleApp2/Next/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Next;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Podaj liczbe calkowita(n):");
+      double wartosc = double.Parse(Console.ReadLine());
+      int n = (int) wartosc;
+      double wynik = wartosc;
+      Console.WriteLine($"Liczba startowa: {wynik}, Liczba operacji: {n}");
+
+      for (int i = 1; i <= n; i++)
+      {
+          if (i % 2 != 0)
+          {
+              wynik = Math.Pow(wynik, 2);
+              Console.WriteLine($"Wynik {i}: {wynik}");
+          }
+          else
+          {
+              wynik = wynik/2;
+              Console.WriteLine($"Wynik {i}: {wynik}");
+          }
+          Console.WriteLine($"Wynik ostateczny: {wynik}");
+      }
+    }
+
+    
+    
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj numer wyrazu ciagu: (n)");
+        
+       if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+       {
+           int wynik = Rekurencja(n);
+           Console.WriteLine($"Wartosc n-tego wyrazu tego ciagu to: {wynik}");
+       }
+       else
+       {
+           Console.WriteLine("Bledna liczba");
+       }
+       
+       
+    }
+
+    static int Rekurencja(int n)
+    {
+        if (n == 1)
+        {
+            return 2;
+        }
+        else if  (n % 2 == 0)
+        {
+            return Rekurencja(n - 1) * 2;
+        }
+        else
+        {
+            return Rekurencja(n - 1) + 2;
+        }
+    }
+}
