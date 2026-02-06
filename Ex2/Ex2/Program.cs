b4bbbc430862e41e0b9bbd886f84a683e6b9b483
iwxnek <iwxnek@gmail.com>
2026-02-06 18:16:09 +0100

    poprawione

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ex2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Podaj ilosc liczb:");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Wybierz poczatek przedzialu:");
+        int A = int.Parse(Console.ReadLine());
+        Console.WriteLine("Wybierz koniec przedzialu:");
+        int B = int.Parse(Console.ReadLine());
+        if (n > (B - A))
+        {
+            Console.WriteLine("Przedzial zbyt maly");
+            return;
+        }
+
+        int suma = 0;
+        int[] wynik = new int[n];
+        Random rnd = new Random();
+
+        while (suma < n)
+        {
+            int liczba = rnd.Next(A, B + 1);
+            bool exists = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (wynik[i] == liczba)
+                {
+                    exists = true;
+                    break;
+                }
+            }
+
+            if (exists == false)
+            {
+                wynik[suma] = liczba;
+                suma++;
+            }
+
+            Console.WriteLine(string.Join(", ", wynik));
+
+        }
+    }
+}
