5ccde9d0c59f6ba10ca9bc41409c89423fd6d198
iwxnek <iwxnek@gmail.com>
2026-02-06 17:58:53 +0100

    dataurodzenia

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj date urodzenia: yyyy-mm-dd");
+        string input = Console.ReadLine();
+        if (DateTime.TryParse(input, out DateTime date))
+        {
+            DateTime dzis = DateTime.Today;
+            int age = dzis.Year - date.Year;
+            if (date.Date > dzis.AddYears(-age))
+            {
+                age--;
+            }
+            Console.WriteLine("Twoj wiek to:" + age);
+        }
+        else
+        {
+           Console.WriteLine("Bledny format daty"); 
+        }
+    }
+}
